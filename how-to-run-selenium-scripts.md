fb917ac929e09c6b48dad290388c1baac967e77e
Harold Altamirano <haroldoalt@gmail.com>
2024-10-29 22:05:03 -0700

    added dependencies and instructions and updates python files

---
+++ how-to-run-selenium-scripts.md
@@ -0,0 +1,8 @@
+1. open vs code terminal
+2. cd ning_files
+3. pip install -r requirements.txt
+   1. if you don't have python installed please install latest version https://www.python.org/downloads/
+   2. restart computer to have python recognized by terminal
+4. have latest chrome installed (automatically done if installed)
+5. python seleniumLibrary.py
+6. python seleniumLogon.py
--- ning_files/seleniumLogon.py
+++ ning_files/seleniumLogon.py
@@ -2,7 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
-driver = webdriver.Chrome('./drivers/chromedriver')
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_driver_path = './drivers/chromedriver/chromedriver.exe'  # Path to your ChromeDriver
+
+
+# Use the Service class to specify driver path
+service = Service(chrome_driver_path)
+driver = webdriver.Chrome(service=service, options=chrome_options)
+
 driver.get("http://localhost:8069/web/login")
 print(driver.title)
 #search_bar = driver.find_element_by_name("q")
@@ -14,14 +24,14 @@ print("Ning: Step1 passed")
 search_bar = driver.find_element(By.NAME, "login")
 search_bar.clear()
 time.sleep(3)
-search_bar.send_keys("somesite.com")
+search_bar.send_keys("haroldoalt@gmail.com")
 print("Ning: Step2 passed")
 time.sleep(3)
 
 search_bar = driver.find_element(By.NAME, "password")
 print("Ning: Step3 passed")
 time.sleep(3)
-search_bar.send_keys("yourpassword")
+search_bar.send_keys("openpgpwd")
 time.sleep(3)
 #search_bar.clear()
 search_bar.send_keys(Keys.RETURN)
